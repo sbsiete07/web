a01f0ace1644162058302b20755db0fc81150980
sbsiete07 <elozanoh17@gmail.com>
2023-01-09 22:21:04 +0100

    slider projects

--- app/src/app/components/home/home.component.ts
+++ app/src/app/components/home/home.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  constructor(){}
+  ngOnInit(): void {}
+
+  slides: string [] = ['../../../assets/ac.jpg', '../../../assets/zelda.jpg', '../../../assets/mario.jpg' ]
+  lenght = this.slides.length
+  i=0;
+
+  getSlide() {
+    return this.slides[this.i];
+  }
+
+  getPrev() {
+     if(this.i > 0 ){
+       this.i--  
+     }else{
+      this.i = this.lenght - 1
+     }
+ 
+  }
+//edit here    
+  getNext() {
+    if(this.i  < this.slides.length - 1){
+      this.i++;
+    }else{
+      this.i = 0
+    }
+  }
+
 }
